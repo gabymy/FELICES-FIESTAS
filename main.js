10e3ff74da7dee194241e9b027b5eb42d734d13c
Gabriela Mutilva <66925220+gabymy@users.noreply.github.com>
2021-12-08 16:36:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function cargar(){
+
+    const sonidos = document.getElementById('sonidos');
+
+document.addEventListener('keydown', function(evento){
+    if(evento.mousemove == 1){
+        sonidos.innerHTML += '<audio src="./sonido/51705__bristolstories__special3 (1).mp3" autoplay></audio>';
+
+    }
+})
+}
+
+
+
+
+
+
+
